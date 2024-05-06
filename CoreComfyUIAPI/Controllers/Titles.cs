dec15dd97faf74cb4d6633c8b5739283822d89d2
dieselryan <rdissell@gmail.com>
2024-05-06 15:06:38 -0700

    Initial test

---
+++ CoreComfyUIAPI/Controllers/Titles.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreComfyUIAPI.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class Tiles : ControllerBase
+	{
+		private static readonly string[] Summaries = new[]
+		{
+			"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+		};
+
+		private readonly ILogger<WeatherForecastController> _logger;
+
+		public Tiles(ILogger<WeatherForecastController> logger)
+		{
+			_logger = logger;
+		}
+
+		private void populateTitles(RomanTitles rt)
+		{
+			for (int i = 0; i < 10;i++)
+			{
+				Tile t = new Tile();
+				{
+					t.Id = 1;
+					t.Level = 1;
+					t.Parent = 0;
+					t.path = "/test/image" + i.ToString() + ".jpg";
+					t.pro = false;
+					rt.tiles.Add(t);
+				}
+			}
+			
+		}
+		[HttpGet]
+		public RomanTitles Get()
+		{
+			RomanTitles romanTitles= new RomanTitles();
+			populateTitles(romanTitles);
+			return romanTitles;
+		}
+	}
+}
---
+++ CoreComfyUIAPI/Titles.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+
+namespace CoreComfyUIAPI
+{
+	public class RomanTitles
+	{
+		public List<Tile> tiles { get; set; }
+		public RomanTitles()
+		{
+			tiles = new List<Tile>();
+		}
+	}
+
+	public class Tile
+	{
+		public int Level { get; set; }
+		public int Parent { get; set; }
+		public int Id { get; set; }
+		public string path { get; set; }
+		public bool pro { get; set; }
+
+	}
+}
